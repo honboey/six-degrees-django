4e0d223f4045d0625c3843c6878179d77ed60821
Hon Boey <hon@dialogicstudios.com.au>
2023-09-24 14:47:18 +1000

    Add ability to choose two random rappers from list #3

--- sixdegreesgame/views.py
+++ sixdegreesgame/views.py
@@ -1,6 +1,7 @@
 import os
 import spotipy
 import json
+import random
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -8,7 +9,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 # Spotify credentials
 
@@ -18,13 +18,19 @@ client_secret = os.getenv("CLIENT_SECRET")
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+
 # Views
 
 
 def index(request):
-    birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP"
-    context = {"results": spotify.artist_albums(birdy_uri, album_type="album")}
-    print(json.dumps(context, indent=2))
+    # List of rappers to choose from
+    with open("sixdegreesgame/data/rappers.json", "r") as rappers_file:
+        rappers = json.load(rappers_file)
+
+    # Choose two out of the list
+    random_rappers = random.sample(rappers, 2)
+
+    context = {"random_rappers": random_rappers}
     return render(request, "sixdegreesgame/index.html", context)
 
 
