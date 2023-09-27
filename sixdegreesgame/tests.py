b323ac414180613ba92577791b1738b04de73db3
Hon Boey <hon@dialogicstudios.com.au>
2023-09-27 15:14:34 +1000

    Write test for initialising rappers #3 #6

--- sixdegreesgame/tests.py
+++ sixdegreesgame/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
+from .views import initialise_rappers
+
 # Create your tests here.
+
+
+class IndexViewsTests(TestCase):
+    def test_initialise_rappers(self):
+        """
+        Test that we get two rappers from the list and a URL for their image on Spotify
+        """
+        rappers = initialise_rappers()
+
+        # Check that it's a tuple with two dictionaries
+        self.assertIsInstance(rappers, tuple)
+        self.assertEqual(len(rappers), 2)
+        self.assertIsInstance(rappers[0], dict)
+        self.assertIsInstance(rappers[1], dict)
+
+        # Test specific keys or values in the dictionaries
+        self.assertIn("name", rappers[0])
+        self.assertIsInstance(rappers[0]["name"], str)
+        self.assertIn("https:", rappers[0]["image"]["url"])
+        self.assertIn("name", rappers[1])
+        self.assertIsInstance(rappers[1]["name"], str)
+        self.assertIn("https:", rappers[1]["image"]["url"])
--- sixdegreesgame/views.py
+++ sixdegreesgame/views.py
@@ -21,22 +21,21 @@ client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-
 # _ -> Dict, Dict
 def initialise_rappers():
     # List of rappers to choose from
     with open("sixdegreesgame/data/rappers.json", "r") as rappers_file:
         rappers = json.load(rappers_file)
-        
+
     two_random_rappers = random.sample(rappers, 2)
     return (
         {
             "name": two_random_rappers[0],
-            "image": get_rapper_image(two_random_rappers[0])
+            "image": get_rapper_image(two_random_rappers[0]),
         },
         {
             "name": two_random_rappers[1],
-            "image": get_rapper_image(two_random_rappers[1])
+            "image": get_rapper_image(two_random_rappers[1]),
         },
     )
 
@@ -47,21 +46,18 @@ def get_rapper_image(str):
     if len(rapper_details) > 0:
         return rapper_details[0]["images"][0]
 
+
 # Views
 
 
 def index(request):
-
     rappers = initialise_rappers()
 
-    song_1 = Song(name="Song name") 
+    song_1 = Song(name="Song name")
     if request.method == "POST":
         print(request.POST)
         if request.POST.get("form-id") == "song-1":
-            song_1 = Song(
-                name=request.POST.get("song-1")
-                )
-    
+            song_1 = Song(name=request.POST.get("song-1"))
 
     context = {
         "rapper_1": rappers[0],
